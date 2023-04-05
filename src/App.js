802db6c1899bd663d9f0a8411900337dc0fc9a8f
Nico Hämäläinen <nibsitaas@proton.me>
2023-04-05 11:07:02 +0300

    Finished initial notes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-const App = (props) => {
-  const { notes } = props;
+import Note from './components/Note'
 
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note) => (
-          <li>{note.content}</li>
+        {notes.map(note => (
+          <Note key={note.id} note={note} />
         ))}
       </ul>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
